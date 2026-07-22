459a7a36880d77324bce2ea79b568d2afebe678a
harshavardhan-9 <vardhanharsha607@gmail.com>
2026-07-23 01:19:19 +0530

    add manual smoke test

--- src/main/java/com/harsha/minileveldb/Main.java
+++ src/main/java/com/harsha/minileveldb/Main.java
@@ -1,7 +1,11 @@
 package com.harsha.minileveldb;
 
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        LevelDBImpl db = new LevelDBImpl("data");
+        db.put("a", "1");
+        System.out.println("get a -> " + db.get("a")); // expect 1
+        db.delete("a");
+        System.out.println("get a after delete -> " + db.get("a")); // expect null
     }
 }
